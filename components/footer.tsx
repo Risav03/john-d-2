8ba18c7285157d7a5c3ea0318266ceab170be07b
Risav03 <needleweed03@gmail.com>
2024-10-13 11:33:45 +0530

    components

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+// import chumfooter from "@/assets/footerchum.jpg"
+
+export const Footer = () => {
+  return (
+    <div className='bg-[#d55a34] w-screen h-40 flex justify-end px-10'>
+        {/* <Image src={chumfooter} alt='footer' className='w-40' /> */}
+    </div>
+  )
+}
